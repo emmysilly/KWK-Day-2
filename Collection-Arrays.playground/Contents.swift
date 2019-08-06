442911a395fa26fa7dac81cbfbbd8d56d754989f
Emmy <EmmyChavez@Apples-MacBook-Pro-47.local>
2019-08-06 14:22:07 -0400

    Arrays

---
+++ Collection-Arrays.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Lesson 5: Collection - Arrays
+
+// Example of Empy Array
+var arrayOfStrings = [String]()
+var arrayofIntegers = [Int] ()
+
+//Example of array
+var friendsOfKarlie = ["Michelle Obama", "Serena Williams", "Taylor Swift", "Jimmy Fallon"]
+
+//Example of Accessing information
+friendsOfKarlie[2]
+//-> Taylor Swift
+
+friendsOfKarlie[0]
+//-> Michelle Obama
+
